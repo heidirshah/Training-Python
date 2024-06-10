bf1dd5abdcdafd02efc9922443d9c82e14f9220d
heidir <mohdheidir@gmi.edu.my>
2024-06-10 16:14:07 +0800

    update

---
+++ exercise1.py
@@ -0,0 +1,12 @@
+#print number
+
+print("2") #string
+print(2)    #integer
+print(2.0)  #float
+print(.2)   #float
+print(0o123)    #octal decimal
+print(0x6AF5)   #hexadecimal
+print(0.00005)  #exponential
+print(0.000005) #exponential
+print(0.0005)
+

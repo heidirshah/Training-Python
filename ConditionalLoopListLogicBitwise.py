33f0477b980b1a7c3de840ebc5c09df53f18dc52
heidir <mohdheidir@gmi.edu.my>
2024-06-11 11:14:12 +0800

    update

---
+++ ConditionalLoopListLogicBitwise.py
@@ -0,0 +1,12 @@
+#Equality Operator (==)
+#Inequality Operator (!=)
+#Less than (<)
+#Less than or equivalent (<=)
+
+mySpeed = int(input("Enter your speed:"))
+limitSpeed = 110
+
+if mySpeed > limitSpeed:
+    print("You exceeds the speed limit")
+else:
+    print("You are under the speed limit")

70265d6c9dd5f8352ba34b64e2693271a43be627
heidir <mohdheidir@gmi.edu.my>
2024-06-11 12:11:18 +0800

    update

---
+++ forLoop2.py
@@ -0,0 +1,20 @@
+# for i in range (2,10,2):
+#     if i == 6:
+#         break
+#     print("inside for loop", i)
+
+# print("outside for loop",i)
+
+# for i in range (2,20,2):
+#     if i == 6:
+#         continue                    #number 6 will be skipped
+#     print("inside for loop", i)
+
+# print("outside for loop",i)
+
+for i in range (2,20,2):
+    if i == 6 or i == 10:
+        continue                    #number 6 & 10 will be skipped
+    print("inside for loop", i)
+
+print("outside for loop",i)

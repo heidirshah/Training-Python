6a941bdf428f49f2efb536928edb2eb845cd39dd
heidir <mohdheidir@gmi.edu.my>
2024-06-11 16:10:21 +0800

    update

---
+++ TKinterTest.py
@@ -0,0 +1,11 @@
+#Tutorial from https://www.youtube.com/watch?v=yQSEXcf6s2I&list=PLCC34OHNcOtoC6GglhF3ncJ5rLwQrLGnV
+
+from tkinter import *
+
+root = Tk()
+
+#creating a label widget
+myLabel = Label(root, text="Hello World!")
+
+#shoving it onto the screen
+myLabel.pack()

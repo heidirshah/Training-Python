127be7f5ed8b0cb1d48ee368ea1a45561d8a0222
heidir <mohdheidir@gmi.edu.my>
2024-06-11 10:23:24 +0800

    update

---
+++ Variables.py
@@ -0,0 +1,14 @@
+myVar = 20        #camel case
+my_Var = "50"     #snake case
+myFloat = 20.3
+
+#print("Enter any value and press enter:")
+#myInput =  input()  #input() function will be run immediately here
+
+myInput =  input("Enter any value and press enter:")    #simple way to write instruction as line 5-6
+
+print(myVar,my_Var,myFloat, sep="--")
+
+print(myVar*5,my_Var*3,sep="--")    #my_Var will be printed 3 times because it's a string
+
+print("myInput-->",myInput)

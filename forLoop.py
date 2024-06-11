0cfe1f23b3cd148ebe87a770f01b68cdcc64f699
heidir <mohdheidir@gmi.edu.my>
2024-06-11 11:59:27 +0800

    Update

---
+++ forLoop.py
@@ -0,0 +1,14 @@
+# for i in range (5):     #equivalent to (i = 0; i < 5; i++) in c
+#     print(i)
+
+# print("outside of for loop",i)
+
+# for i in range (2,5):     #equivalent to (i = 2; i < 5; i++) in c
+#     print(i)
+
+# print("outside of for loop",i)
+
+for i in range (2,13,5):     #equivalent to (i = 2; i < 13; i+5) in c
+    print(i)
+
+print("outside of for loop",i)

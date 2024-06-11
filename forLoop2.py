e845d06175802c57a2ef81763da1b910dca82469
heidir <mohdheidir@gmi.edu.my>
2024-06-11 12:17:45 +0800

    update

--- forLoop2.py
+++ forLoop2.py
@@ -12,9 +12,16 @@
 
 # print("outside for loop",i)
 
-for i in range (2,20,2):
-    if i == 6 or i == 10:
-        continue                    #number 6 & 10 will be skipped
+# for i in range (2,20,2):
+#     if i == 6 or i == 10:
+#         continue                    #number 6 & 10 will be skipped
+#     print("inside for loop", i)
+
+# print("outside for loop",i)
+
+for i in range (20):
+    if i == 6 or i%2 == 1:
+        continue                    #number 6 & odd will be skipped
     print("inside for loop", i)
 
 print("outside for loop",i)
